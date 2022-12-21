7478e029eda333c0331f8da25ded94d33c1c6f60
Roger Hendricks <hendricks.r@gmail.com>
2022-12-22 07:52:15 +1100

    loaded .env for postgres

--- core/config.py
+++ core/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
-
+load_dotenv()
+print(os.getenv("POSTGRES_USER"))
 
 class Settings:
     PROJECT_NAME:str = "Next CIED"
@@ -13,7 +13,7 @@ class Settings:
 
     POSTGRES_USER : str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
+    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER", "localhost")
     POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
     POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
